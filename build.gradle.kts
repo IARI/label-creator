4ab8422698d94fdac6a2f308190f73e492db2fa8
IARI <julian.jarecki@gmail.com>
2020-06-10 17:57:14 +0200

    updated to gradle 6.5, kotlin 1.3.72, TFXserializer .1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
-val kotlinVersion = "1.3.71"
-val mainClass = "com.julianjarecki.ettiketten.app.EttikettenApp"
+val kotlinVersion = "1.3.72"
+val appMainClassName = "com.julianjarecki.ettiketten.app.EttikettenApp"
 version = "0.0.1"
 
 plugins {
     id("com.github.johnrengelman.shadow") version "5.1.0"
-    id("kotlinx-serialization") version "1.3.71"
-    kotlin("jvm") version "1.3.71"
+    id("kotlinx-serialization") version "1.3.72"
+    kotlin("jvm") version "1.3.72"
 
     application
     id("edu.sc.seis.launch4j") version "2.4.6"
+
+    id("com.github.breadmoirai.github-release") version "2.2.9"
 }
 
 //group 'com.julianjarecki.ettiketten'
@@ -22,41 +24,41 @@ repositories {
 
 dependencies {
     testCompile("io.kotlintest:kotlintest-runner-junit5:3.3.2")
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
 
-    compile("no.tornado:tornadofx:1.7.20")
-    //compile 'org.controlsfx:controlsfx:9.0.0'
-    //compile("org.controlsfx:controlsfx:8.40.15")
+    implementation("no.tornado:tornadofx:1.7.20")
+    //implementation("org.controlsfx:controlsfx:9.0.0")
+    //implementation("org.controlsfx:controlsfx:8.40.15")
 
     // use tfx serializer to serialize javafx prooperties
-    compile("com.julianjarecki:TFXserializer:1.0.0-alpha-1")
+    implementation("com.julianjarecki:TFXserializer:1.0.1")
 
     //compile 'de.jensd:fontawesomefx:8.15.0'
-    compile("de.jensd:fontawesomefx-commons:8.15")
-    compile("de.jensd:fontawesomefx-controls:8.15")
-    compile("de.jensd:fontawesomefx-fontawesome:4.7.0-5")
-    compile("de.jensd:fontawesomefx-materialdesignfont:1.7.22-4")
-    compile("de.jensd:fontawesomefx-materialicons:2.2.0-5")
-    //compile("de.jensd:fontawesomefx-emojione:2.2.7-2")
-    //compile("de.jensd:fontawesomefx-icons525:3.0.0-4")
-    //compile("de.jensd:fontawesomefx-octicons:4.3.0-5")
-    //compile("de.jensd:fontawesomefx-weathericons:2.0.10-5")
+    implementation("de.jensd:fontawesomefx-commons:8.15")
+    implementation("de.jensd:fontawesomefx-controls:8.15")
+    implementation("de.jensd:fontawesomefx-fontawesome:4.7.0-5")
+    implementation("de.jensd:fontawesomefx-materialdesignfont:1.7.22-4")
+    implementation("de.jensd:fontawesomefx-materialicons:2.2.0-5")
+    //implementation("de.jensd:fontawesomefx-emojione:2.2.7-2")
+    //implementation("de.jensd:fontawesomefx-icons525:3.0.0-4")
+    //implementation("de.jensd:fontawesomefx-octicons:4.3.0-5")
+    //implementation("de.jensd:fontawesomefx-weathericons:2.0.10-5")
 
-    //compile("org.apache.poi:poi:4.1.0")
-    //compile("org.apache.poi:poi-ooxml:4.1.0")
-    //compile("org.apache.xmlgraphics:batik-all:1.10")
-    //compile("com.github.afester.javafx:FranzXaver:0.1")
+    //implementation("org.apache.poi:poi:4.1.0")
+    //implementation("org.apache.poi:poi-ooxml:4.1.0")
+    //implementation("org.apache.xmlgraphics:batik-all:1.10")
+    //implementation("com.github.afester.javafx:FranzXaver:0.1")
 
-    //compile("com.itextpdf:itextpdf:5.5.13.1")
-    compile("com.itextpdf:kernel:7.1.8")
-    //compile("com.itextpdf:io:7.1.8")
-    compile("com.itextpdf:layout:7.1.8")
-    //compile("com.itextpdf:forms:7.1.8")
-    //compile("com.itextpdf:pdfs:7.1.8")
+    //implementation("com.itextpdf:itextpdf:5.5.13.1")
+    implementation("com.itextpdf:kernel:7.1.8")
+    //implementation("com.itextpdf:io:7.1.8")
+    implementation("com.itextpdf:layout:7.1.8")
+    //implementation("com.itextpdf:forms:7.1.8")
+    //implementation("com.itextpdf:pdfs:7.1.8")
 
     // https://mvnrepository.com/artifact/net.java.dev.jna/jna
-    //compile("net.java.dev.jna:jna:5.4.0")
+    //implementation("net.java.dev.jna:jna:5.4.0")
 }
 
 tasks {
@@ -69,7 +71,7 @@ tasks {
     }
 
     application {
-        mainClassName = mainClass
+        mainClassName = appMainClassName
     }
 
     sourceSets {
@@ -85,13 +87,13 @@ tasks {
     jar {
         manifest {
             attributes["Class-Path"] = configurations.compile.get().all.map { it.name }.joinToString(" ")
-            attributes["Main-Class"] = mainClass
+            attributes["Main-Class"] = appMainClassName
         }
     }
 
     launch4j {
         outfile = "Label Creator.exe"
-        mainClassName = mainClass
+        mainClassName = appMainClassName
         icon = "${projectDir}/resources/icons/label.ico"
         productName = "Label Creator"
         val shadowJar = project.tasks.shadowJar.get()
@@ -101,5 +103,13 @@ tasks {
 
     test {
         useJUnitPlatform()
+    }
+
+    githubRelease {
+        repo("label-creator")
+        owner("IARI")
+        //targetCommitish("master")
+        overwrite(true)
+        draft(true)
     }
 }
